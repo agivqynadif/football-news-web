f18aba568d79607636039033979697b63926e84a
Agivqy Nadif <agivqynadif@gmail.com>
2024-08-16 14:57:43 +0700

    feat: add ArticleNews resource

--- app/Models/League.php
+++ app/Models/League.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -16,6 +17,12 @@ class League extends Model
         'slug',
         'icon',
     ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 
     public function articleNews(): HasMany
     {
