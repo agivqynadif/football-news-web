18a60538ee0a04ed8fb766d6ccc36d0998ae5ecc
Agivqy Nadif <agivqynadif@gmail.com>
2024-08-21 00:14:40 +0700

    feat: add data -> ArticleNews, Author, and BannerAdsvertisement

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ArticleNews;
+use App\Models\Author;
+use App\Models\BannerAdvertisement;
 use App\Models\League;
 use Illuminate\Http\Request;
 
@@ -11,6 +14,27 @@ class FrontController extends Controller
     public function index()
     {
         $leagues = League::all();
-        return view('front.index', compact('leagues'));
+
+        $articles = ArticleNews::with(['league'])
+            ->where('is_featured', 'not_featured')
+            ->latest()
+            ->take(3)
+            ->get();
+
+        $featured_articles = ArticleNews::with(['league'])
+            ->where('is_featured', 'featured')
+            ->inRandomOrder()
+            ->take(3)
+            ->get();
+
+        $authors = Author::all();
+
+        $bannerads = BannerAdvertisement::where('is_active', 'active')
+            ->where('type', 'banner')
+            ->inRandomOrder()
+            // ->take(1)
+            ->first();
+
+        return view('front.index', compact('leagues', 'articles', 'authors', 'featured_articles', 'bannerads'));
     }
 }
--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -38,7 +38,7 @@
     <nav id="League" class="max-w-[1130px] mx-auto flex justify-center items-center gap-4 mt-[30px]">
 
         @foreach($leagues as $league)
-        <a href="categoryPage.html" class="rounded-full p-[12px_22px] flex gap-[10px] font-semibold transition-all duration-300 border border-[#EEF0F7] hover:ring-2 hover:ring-[#FF6B18]">
+        <a href="{{route('front.league', $league->slug)}}" class="rounded-full p-[12px_22px] flex gap-[10px] font-semibold transition-all duration-300 border border-[#EEF0F7] hover:ring-2 hover:ring-[#FF6B18]">
             <div class="w-6 h-6 flex shrink-0">
                 <img src="{{Storage::url($league->icon)}}" alt="icon" />
             </div>
@@ -48,15 +48,16 @@
 
     </nav>
     <section id="Featured" class="mt-[30px]">
+        @forelse($featured_articles as $article)
         <div class="main-carousel w-full">
             <div class="featured-news-card relative w-full h-[550px] flex shrink-0 overflow-hidden">
-                <img src="assets/images/thumbnails/th-featured-1.png" class="thumbnail absolute w-full h-full object-cover" alt="icon" />
+                <img src="{{Storage::url($article->thumbnail)}}" class="thumbnail absolute w-full h-full object-cover" alt="icon" />
                 <div class="w-full h-full bg-gradient-to-b from-[rgba(0,0,0,0)] to-[rgba(0,0,0,0.9)] absolute z-10"></div>
                 <div class="card-detail max-w-[1130px] w-full mx-auto flex items-end justify-between pb-10 relative z-20">
                     <div class="flex flex-col gap-[10px]">
                         <p class="text-white">Featured</p>
-                        <a href="details.html" class="font-bold text-4xl leading-[45px] text-white two-lines hover:underline transition-all duration-300">Generation Z Prefer Remote Working Than Big Salary</a>
-                        <p class="text-white">12 Jun, 2024 • Business</p>
+                        <a href="{{route('front.details', $article->slug)}}" class="font-bold text-4xl leading-[45px] text-white two-lines hover:underline transition-all duration-300">{{$article->name}}</a>
+                        <p class="text-white">{{$article->created_at->format('M d, Y')}} • {{$article->league->name}}</p>
                     </div>
                     <div class="prevNextButtons flex items-center gap-4 mb-[60px]">
                         <button class="button--previous appearance-none w-[38px] h-[38px] flex items-center justify-center rounded-full shrink-0 ring-1 ring-white hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300">
@@ -74,8 +75,8 @@
                 <div class="card-detail max-w-[1130px] w-full mx-auto flex items-end justify-between pb-10 relative z-20">
                     <div class="flex flex-col gap-[10px]">
                         <p class="text-white">Featured</p>
-                        <a href="details.html" class="font-bold text-4xl leading-[45px] text-white two-lines hover:underline transition-all duration-300">Generation Z Prefer Remote Working Than Big Salary</a>
-                        <p class="text-white">12 Jun, 2024 • Business</p>
+                        <a href="details.html" class="font-bold text-4xl leading-[45px] text-white two-lines hover:underline transition-all duration-300">Aston Villa : Jalan Menuju Papan Atas Liga Inggris</a>
+                        <p class="text-white">12 Jun, 2024 • Premier League</p>
                     </div>
                     <div class="prevNextButtons flex items-center gap-4 mb-[60px]">
                         <button class="button--previous appearance-none w-[38px] h-[38px] flex items-center justify-center rounded-full shrink-0 ring-1 ring-white hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300">
@@ -93,8 +94,8 @@
                 <div class="card-detail max-w-[1130px] w-full mx-auto flex items-end justify-between pb-10 relative z-20">
                     <div class="flex flex-col gap-[10px]">
                         <p class="text-white">Featured</p>
-                        <a href="details.html" class="font-bold text-4xl leading-[45px] text-white two-lines hover:underline transition-all duration-300">Generation Z Prefer Remote Working Than Big Salary</a>
-                        <p class="text-white">12 Jun, 2024 • Business</p>
+                        <a href="details.html" class="font-bold text-4xl leading-[45px] text-white two-lines hover:underline transition-all duration-300">Aston Villa : Jalan Menuju Papan Atas Liga Inggris</a>
+                        <p class="text-white">12 Jun, 2024 • Premier League</p>
                     </div>
                     <div class="prevNextButtons flex items-center gap-4 mb-[60px]">
                         <button class="button--previous appearance-none w-[38px] h-[38px] flex items-center justify-center rounded-full shrink-0 ring-1 ring-white hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300">
@@ -107,52 +108,35 @@
                 </div>
             </div>
         </div>
+        @empty
+        <p>belum ada data terbaru...</p>
+        @endforelse
     </section>
     <section id="Up-to-date" class="max-w-[1130px] mx-auto flex flex-col gap-[30px] mt-[70px]">
         <div class="flex justify-between items-center">
             <h2 class="font-bold text-[26px] leading-[39px]">
                 Latest Hot News <br />
-                Good for Curiousity
+                Top Football League in Europe
             </h2>
             <p class="badge-orange rounded-full p-[8px_18px] bg-[#FFECE1] font-bold text-sm leading-[21px] text-[#FF6B18] w-fit">UP TO DATE</p>
         </div>
         <div class="grid grid-cols-3 gap-[30px]">
-            <a href="details.html" class="card-news">
+            @forelse($articles as $article)
+            <a href="{{route('front.details', $article->slug)}}" class="card-news">
                 <div class="rounded-[20px] ring-1 ring-[#EEF0F7] p-[26px_20px] flex flex-col gap-4 hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300 bg-white">
                     <div class="thumbnail-container w-full h-[200px] rounded-[20px] flex shrink-0 overflow-hidden relative">
-                        <p class="badge-white absolute top-5 left-5 rounded-full p-[8px_18px] bg-white font-bold text-xs leading-[18px]">SPORT</p>
-                        <img src="assets/images/thumbnails/th-cyclist.png" class="object-cover w-full h-full" alt="thumbnail" />
+                        <p class="badge-white absolute top-5 left-5 rounded-full p-[8px_18px] bg-white font-bold text-xs leading-[18px]">{{$article->league->name}}</p>
+                        <img src="{{Storage::url($article->thumbnail)}}" class="object-cover w-full h-full" alt="thumbnail" />
                     </div>
                     <div class="card-info flex flex-col gap-[6px]">
-                        <h3 class="font-bold text-lg leading-[27px]">Bersepeda dapat membuat diri menjadi lebih baik lagi</h3>
-                        <p class="text-sm leading-[21px] text-[#A3A6AE]">12 Jun, 2024</p>
+                        <h3 class="font-bold text-lg leading-[27px]">{{$article->name}}</h3>
+                        <p class="text-sm leading-[21px] text-[#A3A6AE]">{{$article->created_at->format('M d, Y')}}</p>
                     </div>
                 </div>
             </a>
-            <a href="details.html" class="card-news">
-                <div class="rounded-[20px] ring-1 ring-[#EEF0F7] p-[26px_20px] flex flex-col gap-4 hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300 bg-white">
-                    <div class="thumbnail-container w-full h-[200px] rounded-[20px] flex shrink-0 overflow-hidden relative">
-                        <p class="badge-white absolute top-5 left-5 rounded-full p-[8px_18px] bg-white font-bold text-xs leading-[18px]">ENTERTAINMENT</p>
-                        <img src="assets/images/thumbnails/camp.png" class="object-cover w-full h-full" alt="thumbnail" />
-                    </div>
-                    <div class="card-info flex flex-col gap-[6px]">
-                        <h3 class="font-bold text-lg leading-[27px]">Beberapa artis ini merayakan ultah di tengah hutan raya</h3>
-                        <p class="text-sm leading-[21px] text-[#A3A6AE]">12 Jun, 2024</p>
-                    </div>
-                </div>
-            </a>
-            <a href="details.html" class="card-news">
-                <div class="rounded-[20px] ring-1 ring-[#EEF0F7] p-[26px_20px] flex flex-col gap-4 hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300 bg-white">
-                    <div class="thumbnail-container w-full h-[200px] rounded-[20px] flex shrink-0 overflow-hidden relative">
-                        <p class="badge-white absolute top-5 left-5 rounded-full p-[8px_18px] bg-white font-bold text-xs leading-[18px]">POLITIC</p>
-                        <img src="assets/images/thumbnails/th-demonstration.png" class="object-cover w-full h-full" alt="thumbnail" />
-                    </div>
-                    <div class="card-info flex flex-col gap-[6px]">
-                        <h3 class="font-bold text-lg leading-[27px]">Terjadi demo pada ibu kota jakarta membuat macet parah</h3>
-                        <p class="text-sm leading-[21px] text-[#A3A6AE]">12 Jun, 2024</p>
-                    </div>
-                </div>
-            </a>
+            @empty
+            <p>belum ada data terbaru...</p>
+            @endforelse
         </div>
     </section>
     <section id="Best-authors" class="max-w-[1130px] mx-auto flex flex-col gap-[30px] mt-[70px]">
@@ -164,79 +148,28 @@
             </h2>
         </div>
         <div class="grid grid-cols-6 gap-[30px]">
-            <a href="author.html" class="card-authors">
+            @forelse($authors as $author)
+            <a href="{{route('front.author', $author->slug)}}" class="card-authors">
                 <div class="rounded-[20px] border border-[#EEF0F7] p-[26px_20px] flex flex-col items-center gap-4 hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300">
                     <div class="w-[70px] h-[70px] flex shrink-0 rounded-full overflow-hidden">
-                        <img src="assets/images/photos/photo.png" class="object-cover w-full h-full" alt="avatar" />
+                        <img src="{{Storage::url($author->avatar)}}" class="object-cover w-full h-full" alt="avatar" />
                     </div>
                     <div class="flex flex-col gap-1 text-center">
-                        <p class="font-semibold">Junior Parrito</p>
-                        <p class="text-sm leading-[21px] text-[#A3A6AE]">3,492 News</p>
+                        <p class="font-semibold">{{$author->name}}</p>
+                        <p class="text-sm leading-[21px] text-[#A3A6AE]">{{$author->articleNews->count ()}} News</p>
                     </div>
                 </div>
             </a>
-            <a href="author.html" class="card-authors">
-                <div class="rounded-[20px] border border-[#EEF0F7] p-[26px_20px] flex flex-col items-center gap-4 hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300">
-                    <div class="w-[70px] h-[70px] flex shrink-0 rounded-full overflow-hidden">
-                        <img src="assets/images/photos/photo1.png" class="object-cover w-full h-full" alt="avatar" />
-                    </div>
-                    <div class="flex flex-col gap-1 text-center">
-                        <p class="font-semibold">Sabrina Juli</p>
-                        <p class="text-sm leading-[21px] text-[#A3A6AE]">312 News</p>
-                    </div>
-                </div>
-            </a>
-            <a href="author.html" class="card-authors">
-                <div class="rounded-[20px] border border-[#EEF0F7] p-[26px_20px] flex flex-col items-center gap-4 hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300">
-                    <div class="w-[70px] h-[70px] flex shrink-0 rounded-full overflow-hidden">
-                        <img src="assets/images/photos/photo2.png" class="object-cover w-full h-full" alt="avatar" />
-                    </div>
-                    <div class="flex flex-col gap-1 text-center">
-                        <p class="font-semibold">Ijan Malawi</p>
-                        <p class="text-sm leading-[21px] text-[#A3A6AE]">44 News</p>
-                    </div>
-                </div>
-            </a>
-            <a href="author.html" class="card-authors">
-                <div class="rounded-[20px] border border-[#EEF0F7] p-[26px_20px] flex flex-col items-center gap-4 hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300">
-                    <div class="w-[70px] h-[70px] flex shrink-0 rounded-full overflow-hidden">
-                        <img src="assets/images/photos/photo3.png" class="object-cover w-full h-full" alt="avatar" />
-                    </div>
-                    <div class="flex flex-col gap-1 text-center">
-                        <p class="font-semibold">Rita Putria</p>
-                        <p class="text-sm leading-[21px] text-[#A3A6AE]">15,398 News</p>
-                    </div>
-                </div>
-            </a>
-            <a href="author.html" class="card-authors">
-                <div class="rounded-[20px] border border-[#EEF0F7] p-[26px_20px] flex flex-col items-center gap-4 hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300">
-                    <div class="w-[70px] h-[70px] flex shrink-0 rounded-full overflow-hidden">
-                        <img src="assets/images/photos/photo4.png" class="object-cover w-full h-full" alt="avatar" />
-                    </div>
-                    <div class="flex flex-col gap-1 text-center">
-                        <p class="font-semibold">Shayna Xin</p>
-                        <p class="text-sm leading-[21px] text-[#A3A6AE]">3,492 News</p>
-                    </div>
-                </div>
-            </a>
-            <a href="author.html" class="card-authors">
-                <div class="rounded-[20px] border border-[#EEF0F7] p-[26px_20px] flex flex-col items-center gap-4 hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300">
-                    <div class="w-[70px] h-[70px] flex shrink-0 rounded-full overflow-hidden">
-                        <img src="assets/images/photos/photo5.png" class="object-cover w-full h-full" alt="avatar" />
-                    </div>
-                    <div class="flex flex-col gap-1 text-center">
-                        <p class="font-semibold">Dhi Pakao</p>
-                        <p class="text-sm leading-[21px] text-[#A3A6AE]">223 News</p>
-                    </div>
-                </div>
-            </a>
+            @empty
+            <p>belum ada data terbaru...</p>
+            @endforelse
         </div>
     </section>
     <section id="Advertisement" class="max-w-[1130px] mx-auto flex justify-center mt-[70px]">
         <div class="flex flex-col gap-3 shrink-0 w-fit">
-            <a href="#">
+            <a href="{{$bannerads->link}}">
                 <div class="w-[900px] h-[120px] flex shrink-0 border border-[#EEF0F7] rounded-2xl overflow-hidden">
-                    <img src="assets/images/iklans/bannerWide1.png" class="object-cover w-full h-full" alt="ads" />
+                    <img src="{{Storage::url($bannerads->thumbnail)}}" class="object-cover w-full h-full" alt="ads" />
                 </div>
             </a>
             <p class="font-medium text-sm leading-[21px] text-[#A3A6AE] flex gap-1">
